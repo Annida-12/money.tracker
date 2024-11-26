750819107e05985304987e9e90e4a2fa19f9205e
Annida-12 <nidaanm12@gmail.com>
2024-11-27 01:14:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require("express");
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to Money Tracker API");
 });
 
-app.get("/api/transactions", (req, res) => {
-    res.json([
-        { id: 1, description: "Buy groceries", amount: 50 },
-        { id: 2, description: "Book purchase", amount: 30 },
-    ]);
-});
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from the backend!" });
+  });
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => console.log("Server running!"));
+app.listen(PORT, () => console.log(`Backend berjalan di port ${PORT}`));
 
